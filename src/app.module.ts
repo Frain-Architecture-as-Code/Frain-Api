fb930caeb010ad929ea3afefe8b426a3be844565
JonatanFD <u20211b995@upc.edu.pe>
2026-02-21 03:20:42 -0500

    feat: add notifications module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { OrganizationsModule } from './organizations/organizations.module';
 import { ProjectsModule } from './projects/projects.module';
+import { NotificationsModule } from './notifications/notifications.module';
 
 @Module({
-  imports: [OrganizationsModule, ProjectsModule],
+  imports: [OrganizationsModule, ProjectsModule, NotificationsModule],
   controllers: [],
   providers: [],
 })
