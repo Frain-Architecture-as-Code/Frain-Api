1a53ad563ee8d1b47f4c77c549a46f576d883a14
JonatanFD <u20211b995@upc.edu.pe>
2026-02-22 00:53:12 -0500

    feat: add swagger

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { cleanupOpenApiDoc } from 'nestjs-zod';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ConsoleLogger } from '@nestjs/common';
+import { ConsoleLogger, Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
@@ -14,13 +14,19 @@ async function bootstrap() {
   const openApiDoc = SwaggerModule.createDocument(
     app,
     new DocumentBuilder()
-      .setTitle('Example API')
-      .setDescription('Example API description')
+      .setTitle('Frain API')
+      .setDescription('Infrastructure as Code')
       .setVersion('1.0')
+      .addBearerAuth()
       .build(),
   );
 
   SwaggerModule.setup('api', app, cleanupOpenApiDoc(openApiDoc));
+
+  const logger = new Logger('Main');
+  logger.log('---- Application started ----');
+  logger.log('Swagger Docs: http://localhost:3000/api');
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
